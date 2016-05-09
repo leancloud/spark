6b433ad8a4e988229bb8c03195093014d511cf7f
junwen <jwfing@gmail.com>
2016-05-09 17:50:29 +0800

    modify build script

--- leanbuild.sh
+++ leanbuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export MAVEN_OPTS="-Xmx2g -XX:MaxPermSize=1024M -XX:ReservedCodeCacheSize=1024m"
-mvn -e -Pspark-ganglia-lgpl -Pyarn -Psparkr -Phive -Dhadoop.version=2.3.0-cdh5.0.5 -Dprotobuf.version=2.5.0 -DskipTests clean package
+#mvn -e -Pspark-ganglia-lgpl -Pyarn -Psparkr -Phive -Dhadoop.version=2.3.0-cdh5.0.5 -Dprotobuf.version=2.5.0 -DskipTests clean package
+mvn -e -Pspark-ganglia-lgpl -Pyarn -Phive -Dhadoop.version=2.3.0-cdh5.0.5 -Dprotobuf.version=2.5.0 -DskipTests clean package
